a7508f24164a10c4573f5224ebce3b9e7cad508e
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2024-09-02 00:43:12 +0200

    Improve tqdm name

--- densify.py
+++ densify.py
@@ -269,7 +269,7 @@ def densify(sequence_path: Path):
         sequence_path=sequence_path,
     )
     timestep_view_buffer = []
-    for i in tqdm(range(30_000), desc=f"timestep {timestep}"):
+    for i in tqdm(range(30_000), desc=f"Densifying"):
         view = get_random_element(
             input_list=timestep_view_buffer, fallback_list=timestep_views
         )
