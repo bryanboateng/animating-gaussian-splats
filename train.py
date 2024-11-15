b1ed6b9f9f78ef72dd9683f2bb1fe205eb11694f
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2024-11-15 13:53:41 +0100

    Add gradient norm clipping

--- train.py
+++ train.py
@@ -765,6 +765,7 @@ def train(config: Config):
             )
 
             loss.backward()
+            nn.utils.clip_grad_norm(deformation_network.parameters(), max_norm=1.0)
 
             wandb.log(
                 {"gradient-norm": calculate_gradient_norm(deformation_network)},
